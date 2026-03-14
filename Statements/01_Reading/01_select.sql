98aa1779642b9fcc4fcbd05865d9dcc00301d445
vicHugo76 <viccorrt@gmail.com>
2026-03-13 19:03:09 -0700

    Aprendiendo SQL

---
+++ Statements/01_Reading/01_select.sql
@@ -0,0 +1,10 @@
+-- SELECT
+
+-- Obtiene todos los datos de la tabla "employees"
+SELECT * FROM employees;
+
+-- Obtiene todos los nombres de la tabla "employees"
+SELECT FirstName FROM employees;
+
+-- Obtiene todos los identificadores y nombres de la tabla "employees"
+SELECT IdEmp, FirstName FROM employees;
