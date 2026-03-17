5f05d3d38249344f145310628f789091bef17140
vicHugo76 <viccorrt@gmail.com>
2026-03-16 20:24:03 -0700

    Aprendiendo SQL

---
+++ Statements/01_Reading/07_limit.sql
@@ -0,0 +1,7 @@
+-- LIMIT
+
+-- Obtiene las 3 primeras filas de la tabla "employees"
+SELECT * FROM employees LIMIT 3;
+
+-- Obtiene las 2 primeras filas de la tabla "employees" con Salary no mayor a 8000 o del departamento 4
+SELECT * FROM employees WHERE NOT Salary > 8000 OR DeptId = 4 LIMIT 2;
